595ed6f2130212032d5adcfd00d9b22b56b51cf6
Akhil <akhil.d@bridge-india.in>
2017-12-16 01:24:42 +0530

    Factory Method moved

--- Abstract Factory pattern - 2/_Main.cs
+++ Abstract Factory pattern - 2/_Main.cs
@@ -4,17 +4,27 @@ namespace BridgeTechWhizz
 {
     public class MainApp
     {
-    
+
         // Client program
         public static void Main()
         {
             while (true)
             {
-                Console.WriteLine("Enter Connection choice?");
+                Console.WriteLine("Database Connection");
+                Console.WriteLine("-------------------");
+                Console.WriteLine("1. OleDb Database");
+                Console.WriteLine("2. Sql server Database");
+                Console.Write("Enter choice:");
                 var input = Console.ReadLine();
                 IDatabase database = DatabaseFactory.GetDatabaseObject(input); // Main Factory provides the database object
+                if (database == null)
+                {
+                    Console.WriteLine("Invalid choice. Press any key to exit");
+                    Console.ReadKey();
+                    break;
+                }
                 database.PrintConnection();
-                Console.WriteLine();
+                Console.Write("\n\n");
             }
         }
     }
